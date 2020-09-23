30b87419991ca93dccdeecc6bf0bf7cb845867c0
Quan <qle001@plattsburgh.edu>
2020-09-22 20:21:35 -0400

    commit 3

--- react-practice-firt-file/my-app/src/App.js
+++ react-practice-firt-file/my-app/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
+import Cow from "./images/cow.gif"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Button, Alert, Breadcrumb, BreadcrumbItem, Card } from 'react-bootstrap'
 
 function App() {
   return (
-    <div>
-      <p>
-        Hello world
-      </p>
+    <div className="App">
+      <header className="App-header">
+        <Card>
+          <Card.Body>
+            <Card.Img />
+            <Card.Title style={{color: "red"}}>Shinano</Card.Title>
+          </Card.Body>
+        </Card>
+
+        <Alert variant="primary">this is a Cow</Alert>
+        <Button className="btn btn-light">click me</Button>
+        <Breadcrumb>
+          <BreadcrumbItem>test 1</BreadcrumbItem>
+          <BreadcrumbItem>test 2</BreadcrumbItem>
+          <BreadcrumbItem active>test 3</BreadcrumbItem>
+        </Breadcrumb>
+      </header>
     </div>
   );
 }
